cd30b4c80c7d8136b4e04d1d0a29745e56237c8f
anonimoDoGithub <xxexpgamerxx@gmail.com>
2023-11-25 22:23:52 -0300

    hello world

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
-echo 'Hello World!';
+echo 'Hello World!'."<br/>"; // entre aspas simples 'valor literal' ou seja uma string.
+
+$nome = 'Sergio';
+$sobrenome = 'Felipe';
+
+$nomeCompleto = "$nome $sobrenome"; // entre aspas duplas, verifica se tetm alguma variavel a ser execultada.
+
+$listaDePresentes = ['Smartphone', 'Tablet', 'Laptop', 'Drone', 'Smartwatch']; // Array
+
+$pessoaPresenteada = "$nomeCompleto ganho um $listaDePresentes[0] de presente.";
+echo $pessoaPresenteada;
