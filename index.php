e3a701724d455f608d0d9beead8431ad2c6e4222
anonimoDoGithub <xxexpgamerxx@gmail.com>
2023-11-25 23:55:14 -0300

    hello world

--- index.php
+++ index.php
@@ -8,5 +8,7 @@ $nomeCompleto = "$nome $sobrenome"; // entre aspas duplas, verifica se tem algum
 
 $listaDePresentes = ['Smartphone', 'Tablet', 'Laptop', 'Drone', 'Smartwatch']; // Array
 
-$pessoaPresenteada = "$nomeCompleto ganho um $listaDePresentes[0] de presente.";
-echo $pessoaPresenteada;
+$itemDaListaDePresentes = [...$listaDePresentes, 'Xiaomi']; // Operador Array Spread Obs: PHP 7.4 "um atalho para COPIAR um Array e coloca-lo dentro de outro...
+
+$pessoaPresenteada = "$nomeCompleto ganho um $listaDePresentes[0] de presente, da marca $itemDaListaDePresentes[5].";
+echo $pessoaPresenteada."<br/>";
